fe4138b6ecac757adb06b4945a29b866a1603fb4
Owen Moss <owenkmoss@gmail.com>
2018-05-28 20:44:04 +0200

    started react advanced plus last lecture

---
+++ src/components/gif.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+  if (!this.props.id) {
+    return null;
+  }
+
+    const src = `https://media0.giphy.com/media/${this.props.id}/200w.gif`;
+    return (
+      <img src={src} alt="" className="gif" onClick={this.handleClick}/>
+    );
+  }
+}
+
+export default Gif;
